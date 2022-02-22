ba54303b26cafb4fff9922f7eb7ced195f638b3f
vaansuit <vvansuit@outlook.com>
2022-02-21 22:23:43 -0300

    added music in js / bugged yet

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,22 @@
-const song = document.querySelector(".song");
-const gif = document.querySelector(".gif-container video");
-const sounds = document.querySelectorAll(".sound-picker button");
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.picker');
+    const gif = document.querySelector('.gif-container img');
+
+    const sounds = document.querySelectorAll('.picker button');
+
+    const playing = play.addEventListener('click', () => {
+        song.play();
+    });
+
+    sounds.forEach(sound => {
+        SVGFESpecularLightingElement.addEventListener('click', function() {
+            song.src = this.getAttribute('data-sound');
+            gif.src = this.getAttribute('data-image');
+            song.play();
+        });
+    })
+};
+
+
+ app();
